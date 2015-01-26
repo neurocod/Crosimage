0fe7f6843c3d8d3d84b4bfbcbe3c66c95243b53e
neurocod <neurocod@gmail.com>
2015-01-26 18:51:37 +0200

    MicrosoftLnkReader: correct GUID check, QDataStream parsing

--- Crosimage/MicrosoftLnkReader.cpp
+++ Crosimage/MicrosoftLnkReader.cpp
@@ -7,82 +7,64 @@
 #include <string.h>
 #include <sys/stat.h>
 
-template<class T>
-QString MicrosoftLnkReader::printf(const QString & str, T t) {
-	QString ret = str.arg(t);
-	return ret;
-}
+quint8 MicrosoftLnkReader::CorrectGuid[16] = {0x01, 0x14, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46};
 QString MicrosoftLnkReader::targetOrSame(const QString & path) {
 	MicrosoftLnkReader reader;
-	if(!reader.loadAndParse(path))
+	if(!reader.loadAndParse(path).ok())
 		return path;
 	return path;
 }
 StringStatus MicrosoftLnkReader::loadAndParse(const QString & path) {
-	char LinkFileContent[10240];
-	QByteArray fileContent;
-	if(!FileReader::read(path, fileContent))
-		return false;
-	fileContent.append((char)0);
+	QFile file(path);
+	if(!file.open(QIODevice::ReadOnly))
+		return ReadStatus(false, file.errorString());
 
-	int LinkFileSize = fileContent.size();
-	for(int i = 0; i<fileContent.size() && i<sizeof(LinkFileContent); ++i) {
-		LinkFileContent[i] = fileContent.at(i);
-	}
-	_signature = LinkFileContent[0]*65536*256;
-	_signature += LinkFileContent[1]*65536;
-	_signature += LinkFileContent[2]*256+LinkFileContent[3];
-	printf("Signature: %1\n", Signature);
+	QDataStream stream(&file);
+	stream >> _signature;
+	if(_signature!=CorrectSignature)
+		return ReadStatus(false, QString("Unknown link signature: %1, correct is %2").arg(_signature).arg(CorrectSignature));
 
 	for(int i = 0; i<16; i++) {
-		GUID[i] = LinkFileContent[4+i];
+		quint8 ch;
+		stream >> ch;
+		_guid[i] = ch;
+		if(_guid[i]!=CorrectGuid[i])
+			return ReadStatus(false, "Incorrect guid");
 	};
 
-	ShortcutFlags = LinkFileContent[23]*65536*256;
-	ShortcutFlags += LinkFileContent[22]*65536;
-	ShortcutFlags += LinkFileContent[21]*256+LinkFileContent[20];
-	printf("Shortcut Flags: %1\n", ShortcutFlags);
+	stream >> _shortcutFlags;
+	stream >> _targetFileFlags;
+	stream >> _creationTime;
+	stream >> _lastAccessTime;
+	stream >> _lastAccessTime;
 
-	TargetFileFlags = LinkFileContent[27]*65536*256;
-	TargetFileFlags += LinkFileContent[26]*65536;
-	TargetFileFlags += LinkFileContent[25]*256+LinkFileContent[24];
-	printf("Target File Flags: %1\n", TargetFileFlags);
+	stream >> _fileLength;
+	stream >> _iconNumber;
+	stream >> _showWindow;
+	stream >> _hotKey;
 
-	for(int i = 0; i<8; i++) { CreationTime[i] = LinkFileContent[28+i]; };
-	for(int i = 0; i<8; i++) { LastAccessTime[i] = LinkFileContent[36+i]; };
-	for(int i = 0; i<8; i++) { LastAccessTime[i] = LinkFileContent[44+i]; };
-
-	FileLength = LinkFileContent[55]*65536*256;
-	FileLength += LinkFileContent[54]*65536;
-	FileLength += LinkFileContent[53]*256+LinkFileContent[52];
-	printf("Target File Size (bytes): %1\n", FileLength);
-
-	IconNumber = LinkFileContent[59]*65536*256;
-	IconNumber += LinkFileContent[58]*65536;
-	IconNumber += LinkFileContent[57]*256+LinkFileContent[56];
-	printf("IconNumber: %1\n", IconNumber);
-
-	ShowWindow = LinkFileContent[63]*65536*256;
-	ShowWindow += LinkFileContent[62]*65536;
-	ShowWindow += LinkFileContent[61]*256+LinkFileContent[60];
-	printf("ShowWindow Attributes: %1\n", IconNumber);
-
-	HotKey = LinkFileContent[67]*65536*256;
-	HotKey += LinkFileContent[66]*65536;
-	HotKey += LinkFileContent[65]*256+LinkFileContent[64];
-	printf("HotKey Attributes: %1\n", HotKey);
-
-	// Now extract file name and path
-
-	unsigned int OFFSET = 76+(unsigned char)LinkFileContent[76];
-	OFFSET += (unsigned char)LinkFileContent[77]*256+(unsigned char)LinkFileContent[78]+2;
-
-	OFFSET += LinkFileContent[OFFSET];
-	printf("Share name: %1\n", &LinkFileContent[OFFSET]);
-
-	OFFSET += strlen(&LinkFileContent[OFFSET]);
-	OFFSET++;
-
-	printf("File name: %1\n", &LinkFileContent[OFFSET]);
-	return path;
+	QString sharedName= read0Terminated(stream, true);
+	_fileName = read0Terminated(stream, true);
+	return true;
+}
+QString MicrosoftLnkReader::read0Terminated(QDataStream & stream, bool unicode) {
+	QString ret;
+	if(unicode) {
+		while(1) {
+			quint16 ch;
+			stream >> ch;
+			if(!ch || stream.atEnd())
+				break;
+			ret += ch;
+		}
+		return ret;
+	}
+	while(1) {
+		quint8 ch;
+		stream >> ch;
+		if(!ch || stream.atEnd())
+			break;
+		ret += ch;
+	}
+	return ret;
 }
--- Crosimage/MicrosoftLnkReader.h
+++ Crosimage/MicrosoftLnkReader.h
@@ -5,23 +5,25 @@ class MicrosoftLnkReader {
 	public:
 		static QString targetOrSame(const QString & path);
 		StringStatus loadAndParse(const QString & path);
-		template<class T>
-		static QString printf(const QString & str, T t);
 
-		static const int Signature = 0x4C000000;
+		static const int CorrectSignature = 0x4C000000;
 		quint32 _signature;
-		unsigned char GUID[16];//The current GUID for shortcuts is 01 14 02 00 00 00 00 00 C0 00 00 00 00 00 46, It may change in the future.
-		quint32 ShortcutFlags;//Shortcut flags are explained below
-		quint32 TargetFileFlags;//Target file flags - Flags are explained below
-		unsigned char CreationTime[8];//Windows FILETIME structure
-		unsigned char LastAccessTime[8];//FILETIME
-		unsigned char ModificationTime[8];//FILETIME
-		quint32 FileLength;//The length of the target file. 0 if the target is not a file.
+		quint8 _guid[16];
+		static quint8 CorrectGuid[16];
+		quint32 _shortcutFlags;//Shortcut flags are explained below
+		quint32 _targetFileFlags;//Target file flags - Flags are explained below
+		quint64 _creationTime;//Windows FILETIME structure
+		quint64 _lastAccessTime;//FILETIME
+		quint64 _modificationTime;//FILETIME
+		quint32 _fileLength;//The length of the target file. 0 if the target is not a file.
 		//This value is used to find the target when the link is broken.
-		quint32 IconNumber;//If the file has a custom icon (set by the flags bit 6),
+		quint32 _iconNumber;//If the file has a custom icon (set by the flags bit 6),
 		//then this long integer indicates the index of the icon to use. Otherwise it is zero.
-		quint32 ShowWindow;//SW_SHOWNORMAL=1, SW_SHOWMAXIMIZED=3, SW_SHOWMINNOACTIVE=7, all other values MUST be treated as SW_SHOWNORMAL
-		quint32 HotKey;//The hot key assigned for this shortcut
-		quint32 Reserved1;//Reserved - Always 0
-		quint32 Reserved2;//Reserved - Always 0
+		quint32 _showWindow;//SW_SHOWNORMAL=1, SW_SHOWMAXIMIZED=3, SW_SHOWMINNOACTIVE=7, all other values MUST be treated as SW_SHOWNORMAL
+		quint32 _hotKey;//The hot key assigned for this shortcut
+		quint32 _reserved1;//Reserved - Always 0
+		quint32 _reserved2;//Reserved - Always 0
+
+		QString _fileName;
+		static QString read0Terminated(QDataStream & stream, bool unicode);
 };
