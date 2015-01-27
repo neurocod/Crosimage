196d7ef3c7a12028851737ff2c5cf478b783f133
neurocod <neurocod@gmail.com>
2015-01-27 21:54:53 +0200

    ThumbVideoWorker bugs fixed

--- Crosimage/ThumbVideoWorker.cpp
+++ Crosimage/ThumbVideoWorker.cpp
@@ -27,6 +27,7 @@ QImage ThumbVideoWorker::thumbFromVideo(ThumbWorker&worker, const QString & path
 		ASSERT(b);
 	}
 	//there is other adwices (like mencoder) in http://habrahabr.ru/post/171213/
+	//run: "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -ss 00:00:01 -i "video/path.avi" -f image2 -vframes 1 "image/file.jpg"
 	QString program = "C:\\Program Files\\ffmpeg\\bin\\ffmpeg.exe";
 	QTime time(secsOffset/3600, secsOffset/60, secsOffset);
 	QString strTime = time.toString("HH:mm:ss");
@@ -63,22 +64,15 @@ qreal ThumbVideoWorker::colorMonopolization(const QImage & img) {
 	int pixels = cx*cy;
 	if(pixels<=0)
 		return false;
+	const int quantizationBits = 5;
 	const int sz = 512;//2**(3 rgb*3 bits per pixel)
-	QMap<QRgb, int> stat;
 	int colors[sz];
 	for(int & c: colors)
 		c = 0;
-	const int quantizationBits = 5;
-	auto adr = img.scanLine(0);
-	QImage img2 = img;
-	img2.save("d:\\v\\test.jpg");
 	ASSERT(img.format()==QImage::Format_RGB32);
 	for(int y = 0; y<cy; ++y) {
-		auto p = img.scanLine(y);
-		int x = 0;
+		QRgb* p = (QRgb*)img.scanLine(y);
 		for(auto end = p + cx; p<end; ++p) {
-			if(x==100 && y==90)
-				int t = 3;
 			const QRgb rgb = *p;
 			int r = qRed(rgb) >> quantizationBits;
 			int g = qGreen(rgb) >> quantizationBits;
@@ -86,20 +80,12 @@ qreal ThumbVideoWorker::colorMonopolization(const QImage & img) {
 			QRgb rgb2 = r + (g<<3) + (b<<6);
 			ASSERT(rgb2<sz && r<=8 && g<=8 && b<=8);
 			colors[rgb2]++;
-			QRgb rgbPosterized = qRgb(r<<quantizationBits, g<<quantizationBits, b<<quantizationBits);
-			stat[rgbPosterized] = colors[rgb2];
-			img2.setPixel(x, y, rgbPosterized);
-			++x;
 		}
 	}
-	img2.save("d:\\v\\test2.jpg");
 	qSort(colors, colors+sz);
 	int sum = 0;
-	QString str;
 	for(int i = 0; i<3; ++i) {
 		int pixelCount = colors[sz-1-i];
-		QColor clr = stat.keys(pixelCount).first();
-		str += toString(clr) + '\n';
 		sum += pixelCount;
 	}
 	qreal ret = sum;
